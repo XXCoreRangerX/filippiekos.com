1edf6840503aa8df666c479e508c390ccc0c6ed0
XXCoreRangerX <mail@xxcore.pl>
2024-02-19 00:05:21 +0100

    Custom error and 404
    Signed-off-by: XXCoreRangerX <mail@xxcore.pl>

---
+++ app/error.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { XCircle } from "lucide-react";
+import { useEffect } from "react";
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string };
+    reset: () => void;
+}) {
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    return (
+        <div className="flex h-screen flex-col items-center justify-center text-center">
+            <XCircle className="mb-2 h-10 w-10 text-muted-foreground" />
+            <h1 className="text-8xl font-bold">Error</h1>
+            <h2 className="text-2xl">Something went wrong</h2>
+            <Button className="mt-5" onClick={() => reset()}>
+                Refresh
+            </Button>
+        </div>
+    );
+}
