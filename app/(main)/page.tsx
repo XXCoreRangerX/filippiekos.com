d2cd22e76f55f1c6eb342b39db14dcd51469b817
XXCoreRangerX <mail@xxcore.pl>
2024-02-25 20:23:47 +0100

    Remove dynamic load of About section
    Signed-off-by: XXCoreRangerX <mail@xxcore.pl>

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,18 +1,14 @@
+import { About } from "@/app/(main)/_components/about";
 import { Achievements } from "@/app/(main)/_components/achievements";
 import { Articles } from "@/app/(main)/_components/articles";
 import { Blog } from "@/app/(main)/_components/blog";
-import dynamic from "next/dynamic";
 import { GitHubStats } from "./_components/github";
-
-const DynamicAbout = dynamic(() =>
-    import("@/app/(main)/_components/about").then((mod) => mod.About),
-);
 
 export default function Main() {
     return (
         <div className="grid h-full gap-5 p-5 sm:p-8 sm:px-24 lg:flex lg:h-screen lg:p-10">
             <div className="lg:w-1/4">
-                <DynamicAbout />
+                <About />
             </div>
             <div className="flex flex-col gap-5 lg:w-1/2">
                 <Blog />
