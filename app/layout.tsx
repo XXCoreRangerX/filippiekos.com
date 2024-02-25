a68fa57c5a9002b75e51be382281834fc0fb14ee
XXCoreRangerX <mail@xxcore.pl>
2024-02-26 00:40:33 +0100

    Added grid background
    Signed-off-by: XXCoreRangerX <mail@xxcore.pl>

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,12 +57,13 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-            <body className={`${font.className} antialiased`}>
+            <body className={`${font.className} relative antialiased`}>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
                     enableSystem
                 >
+                    <div className="absolute inset-0 -z-[100] bg-[linear-gradient(to_right,#6b7280_1px,transparent_2px),linear-gradient(to_bottom,#6b7280_1px,transparent_2px)] opacity-5 [background-position:10px_10px] [background-size:30px_30px] [mask-image:radial-gradient(ellipse,#000_75%,transparent_100%)]" />
                     {children}
                     <SpeedInsights />
                     <Analytics />
