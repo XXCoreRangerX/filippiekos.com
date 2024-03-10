1aa64db6ad8d45501687841e3c8c3dcad25762be
Filip Piękoś <mail@filippiekos.com>
2024-03-10 18:05:09 +0100

    Fix error styling
    Signed-off-by: Filip Piękoś <mail@filippiekos.com>

--- app/error.tsx
+++ app/error.tsx
@@ -15,13 +15,11 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
     }, [error]);
 
     return (
-        <main className="flex h-screen flex-col items-center justify-center text-center">
+        <main className="flex h-screen flex-col items-center justify-center p-5 text-center">
             <XCircle className="mb-2 h-10 w-10 text-muted-foreground" />
             <h1 className="text-8xl font-bold">Error</h1>
-            <h2 className="text-2xl">Something went wrong</h2>
-            <Button className="mt-5" onClick={() => reset()}>
-                Refresh
-            </Button>
+            <h2 className="mb-2 text-2xl">Something went wrong</h2>
+            <Button onClick={() => reset()}>Refresh</Button>
         </main>
     );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,15 +1,31 @@
+"use client";
+
 import { buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import { XCircle } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Callout } from "@/components/ui/callout";
 
 export default function NotFound() {
+    const path = usePathname();
     return (
-        <main className="flex h-screen flex-col items-center justify-center text-center">
-            <XCircle className="mb-2 h-10 w-10 text-muted-foreground" />
-            <h1 className="text-8xl font-bold">404</h1>
-            <h2 className="text-2xl">Not Found</h2>
-            <Link className={cn("mt-5", buttonVariants())} href="/">
+        <main className="flex h-screen flex-col items-center justify-center p-5 text-center">
+            {path !== "/wp-admin" && (
+                <>
+                    <XCircle className="mb-2 h-10 w-10 text-muted-foreground" />
+                    <h1 className="text-8xl font-bold">404</h1>
+                    <h2 className="mb-2 text-2xl">Not Found</h2>
+                </>
+            )}
+            {path === "/wp-admin" && (
+                <>
+                    <div className="mb-2 animate-spin text-3xl">🤡</div>
+                    <Callout name="Woah there!" variant="danger">
+                        we don&apos;t use WordPress around here lmao
+                    </Callout>
+                </>
+            )}
+            <Link className={buttonVariants()} href="/">
                 Go back
             </Link>
         </main>
